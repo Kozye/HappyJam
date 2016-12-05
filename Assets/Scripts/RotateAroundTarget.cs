a4e686606494fad8f6cd12a54fe10467e7bf9d5f
Toni Steyskal <toni.steyskal@gmail.com>
2016-12-06 00:45:24 +0100

    RotateAroundTarget added

---
+++ Assets/Scripts/RotateAroundTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundTarget : MonoBehaviour
+{
+	public Transform Target;
+	public Vector3 RotationAxis = Vector3.forward;
+	public float RotationAngle = 20.0f;
+
+	private Transform _transform;
+
+	void Awake ()
+	{
+		_transform = transform;
+
+		if (!Target)
+		{
+			Target = _transform;
+			Debug.LogWarning ("Target reference not set, defaulting to object transform.");
+		}
+	}
+
+	void Update ()
+	{
+		_transform.RotateAround (Target.position, RotationAxis, RotationAngle * Time.deltaTime);
+	}
+}
