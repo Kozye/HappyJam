b54daa74f3784820169d013799b8e29766123c4d
Toni Steyskal <toni.steyskal@gmail.com>
2016-12-06 00:31:13 +0100

    HappyFramework added

---
+++ Assets/Scripts/TimedObjectDestructor.cs
@@ -0,0 +1,32 @@
+/*
+ *
+ *	Happy Framework
+ *	by Toni Steyskal, 2016-2017
+ * 
+ */
+
+using UnityEngine;
+
+namespace Happy
+{
+	public class TimedObjectDestructor : MonoBehaviour
+	{
+		[SerializeField]
+		private float _timeOut = 1.0f;
+		[SerializeField]
+		private bool _detachChildren = false;
+
+		private void Awake ()
+		{
+			Invoke ("DestroyNow", _timeOut);
+		}
+
+		private void DestroyNow ()
+		{
+			if (_detachChildren)
+				transform.DetachChildren ();
+
+			DestroyObject (gameObject);
+		}
+	}
+}
