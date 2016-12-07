ac5de59b73ff229be0993ca39294fe7658de7357
Marko Alerić <markoall1@gmail.com>
2016-12-07 23:52:35 +0100

    Added behaviour bricks with example scene with simple AI

---
+++ Assets/Happy Jam/Scripts/AI/BehaviourBricks/SteeringBehaviour/Flee.cs
@@ -0,0 +1,42 @@
+﻿using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
+using UnityEngine;
+using AI;
+
+namespace BBUnity.Actions
+{
+
+    [Action("Steering Behaviour/Flee")]
+    [Help("Steering Behaviour Flee that requires Agent component")]
+    public class Flee : BaseSteeringBehaviour
+    {
+        [InParam("target")]
+        [Help("Target position from which agent will flee")]
+        public Transform target;
+
+        protected Transform _target;
+        protected Transform _transform;
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            _target = target;
+            _transform = gameObject.transform;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            SetSteering();
+            return TaskStatus.RUNNING;
+        }
+        
+        public override Steering GetSteering()
+        {
+            Steering steering = new Steering();
+            steering.linear = _transform.position - _target.position;
+            steering.linear.Normalize();
+            steering.linear = steering.linear * agent.GetMaxAccel();
+            return steering;
+        }
+    }
+}
