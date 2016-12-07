f1c25641b8c78f772fe4f951bec1465d7615281e
Toni Steyskal <toni.steyskal@gmail.com>
2016-12-07 01:31:34 +0100

    Coin Prefab added

--- Assets/Happy Jam/Scripts/GameManager.cs
+++ Assets/Happy Jam/Scripts/GameManager.cs
@@ -6,5 +6,23 @@ using Happy;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+	[Header ("Read-Only")]
+	[SerializeField]
+	private int _coinsCollected = 0;
+	[SerializeField]
+	private int _coinScore = 0;
 
+	public int CoinScore
+	{
+		get
+		{
+			return _coinScore;
+		}
+
+		set
+		{
+			_coinScore += value;
+			_coinsCollected++;
+		}
+	}
 }
